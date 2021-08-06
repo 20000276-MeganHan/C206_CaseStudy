e0941303ab5f07a559e80487176072a8d6d1f3eb
jingr <jingr@DESKTOP-GANQ4GK>
2021-08-06 18:32:45 +0800

    -

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 import org.junit.After;
@@ -14,16 +15,54 @@ public class C206_CaseStudyTest {
 	
 	@Before
 	public void setUp() throws Exception {
+	lunchBoxOrder1 = new Order(1, LocalDate.parse("2021/08/30"), "Carbonara", "Fruit punch", "Watermelon Slice");
+	lunchBoxOrder2 = new Order(2, LocalDate.parse("2021/08/09"), "Carbonara", "Fruit punch", "Watermelon Slice");
+	
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		lunchBoxOrder1 = null;
+		lunchBoxOrder2 = null;
 	}
 
 	@Test
 	public void c206_test() {
 		//fail("Not yet implemented"); 
 		assertTrue("C206_CaseStudy_SampleTest ",true);
+	}
+	
+	public void testAddLunchBoxOrder() {
+		//Order list is not null, so that can add a new order
+		//boundary
+		assertNotNull("Check if there is valid lunchBoxOrder arraylist to add to", orderList); 
+		
+		//Given an empty list, after adding 1 item, the size of the list is 1 - normal
+		//The item just added is as same as the first item of the list
+		//normal
+		orderList.add(lunchBoxOrder1);
+		assertEquals("Check that orderList size is 1", 1, orderList.size());
+		assertSame("Check that lunch box order is added", lunchBoxOrder1, orderList.get(0));
+		
+		//Add another order
+		//Test the size of orderList is 2
+		//The item just added is as same as the second item of the list
+		//normal
+		orderList.add(lunchBoxOrder2);
+		assertEquals("Check that orderList size is 2", 2, orderList.size());
+		assertSame("Check that lunch box order is added", lunchBoxOrder2, orderList.get(1));
+		
+		//Delete order from orderList
+		//Test the size of the list is 1
+		//normal
+		orderList.remove(1);
+		assertEquals("Check that orderList size is 1", 1, orderList.size());
+		
+		//Delete order from orderList
+		//Test the size of the list is 0
+		//normal
+		orderList.remove(0);
+		assertEquals("Check that orderList size is 1", 0, orderList.size());
 	}
 
 }
