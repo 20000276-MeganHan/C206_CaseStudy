25437d9f394655454fa77367066333fb9163028e
jingr <jingr@DESKTOP-GANQ4GK>
2021-08-06 23:14:30 +0800

    test

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -13,18 +13,17 @@ public class C206_CaseStudyTest {
 	private Order lunchBoxOrder2;
 	private Order lunchBoxOrder3;
 	
+	public C206_CaseStudyTest() {
+		super();
+    }
+	
 	@Before
 	public void setUp() throws Exception {
-	lunchBoxOrder1 = new Order(1, LocalDate.parse("2021/08/30"), "Carbonara", "Fruit punch", "Watermelon Slice");
-	lunchBoxOrder2 = new Order(2, LocalDate.parse("2021/09/30"), "Carbonara", "Fruit punch", "Watermelon Slice");
-	lunchBoxOrder3 = new Order(3, LocalDate.parse("2021/08/06"), "Carbonara", "Fruit punch", "Watermelon Slice");
+	lunchBoxOrder1 = new Order(1, LocalDate.parse("2021-08-30"), "Carbonara", "Fruit punch", "Watermelon Slice");
+	lunchBoxOrder2 = new Order(2, LocalDate.parse("2021-08-24"), "Carbonara", "Fruit punch", "Watermelon Slice");
+	lunchBoxOrder3 = new Order(3, LocalDate.parse("2021-08-09"), "Carbonara", "Fruit punch", "Watermelon Slice");
 	}
 
-	@After
-	public void tearDown() throws Exception {
-		lunchBoxOrder1 = null;
-		lunchBoxOrder2 = null;
-	}
 
 	@Test
 	public void c206_test() {
@@ -32,7 +31,9 @@ public class C206_CaseStudyTest {
 		assertTrue("C206_CaseStudy_SampleTest ",true);
 	}
 	
-	public void testAddLunchBoxOrder() {
+
+	@Test
+	public void testLunchBoxOrder() {
 		//Order list is not null, so that can add a new order
 		//boundary
 		assertNotNull("Check if there is valid lunchBoxOrder arraylist to add to", orderList); 
@@ -52,9 +53,7 @@ public class C206_CaseStudyTest {
 		assertEquals("Check that orderList size is 2", 2, orderList.size());
 		assertSame("Check that lunch box order is added", lunchBoxOrder2, orderList.get(1));
 		
-		//Test date entered is after today date
-		//boundary
-		assertEquals("2021-08-07", lunchBoxOrder1.getDate());
+		
 		//Delete order from orderList
 		//Test the size of the list is 1
 		//normal
@@ -65,6 +64,12 @@ public class C206_CaseStudyTest {
 		//Test the size of the list is 0
 		orderList.remove(0);
 		assertEquals("Check that orderList size is 1", 0, orderList.size());
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		lunchBoxOrder1 = null;
+		lunchBoxOrder2 = null;
 	}
 
 }
