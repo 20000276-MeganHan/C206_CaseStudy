8e5533222cd0b1702243f6860535c9b86107c788
jingr <jingr@DESKTOP-GANQ4GK>
2021-08-06 17:01:45 +0800

    -

--- C206_CaseStudy/src/Order.java
+++ C206_CaseStudy/src/Order.java
@@ -1,55 +1,6 @@
 import java.time.LocalDate;
 
 public class Order {
-<<<<<<< HEAD
-	private int id;
-	private LocalDate date;
-	private String meal;
-	private String drink;
-	private String fruit;
-	
-	public Order(int id, LocalDate date, String meal, String drink, String fruit) {
-		this.id = id;
-		this.date = date;
-		this.meal = meal;
-		this.drink = drink;
-		this.fruit = fruit;
-	}
-
-	public String getMeal() {
-		return meal;
-	}
-
-	public void setMeal(String meal) {
-		this.meal = meal;
-	}
-
-	public String getDrink() {
-		return drink;
-	}
-
-	public void setDrink(String drink) {
-		this.drink = drink;
-	}
-
-	public String getFruit() {
-		return fruit;
-	}
-
-	public void setFruit(String fruit) {
-		this.fruit = fruit;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public LocalDate getDate() {
-		return date;
-	}
-	
-=======
-
 	private int id;
 	private LocalDate date;
 	private String meal;
@@ -96,7 +47,6 @@ public class Order {
 		return date;
 	}
 
->>>>>>> branch 'master' of https://github.com/20000276-MeganHan/C206_CaseStudy.git
 	public void printInfo() {
 		String output = String.format("%-5d %-10s %-10s %-10s %s", id, date, meal, drink, fruit);
 		System.out.println(output);
