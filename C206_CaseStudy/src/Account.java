7facb56eb0dd32a6c30c4a75f2887bbc7fb7a401
20029916 <20029916@pritzpc>
2021-08-09 02:04:23 +0800

    Updating a new field in account class

--- C206_CaseStudy/src/Account.java
+++ C206_CaseStudy/src/Account.java
@@ -5,20 +5,23 @@ public class Account {
 	private String user;
 	private double credit;
 	private int studentID;
+	private int mobileNo;
 	
-	public Account(String username, String password, String user, int studentID) {
+	public Account(String username, String password, String user, int studentID, int mobileNo) {
 		this.username = username;
 		this.password = password;
 		this.user = "Parent";
 		this.studentID = studentID;
+		this.mobileNo = mobileNo;
 	}
 	
-	public Account(String username, String password, String user, int studentID, double credit) {
+	public Account(String username, String password, String user, int studentID, double credit, int mobileNo) {
 		this.username = username;
 		this.password = password;
 		this.user = "Student";
 		this.studentID = studentID;
 		this.credit = 0.0;
+		this.mobileNo = mobileNo;
 	}
 
 
@@ -52,6 +55,22 @@ public class Account {
 
 	public int getStudentID() {
 		return studentID;
+	}
+
+	public int getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setStudentID(int studentID) {
+		this.studentID = studentID;
+	}
+
+	public void setMobileNo(int mobileNo) {
+		this.mobileNo = mobileNo;
 	}
 	
 	
